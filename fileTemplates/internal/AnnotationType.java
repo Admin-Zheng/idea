268eb29b5d5298fa569be6e114e00084133744c1
blue_pick <1106687235@qq.com>
2020-12-07 17:10:55 +0800

    IU-2020.2.3 <Supper_Z@LAPTOP-S03111TE Create baseRefactoring.xml, ide.general.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Interface.java, lombok-plugin.xml, yiiguxing.translation.xml, shared-indexes.xml, find.xml, filetypes.xml, xsl.xml, colors.scheme.xml, _@user_Material Oceanic.icls, notifications.xml, databaseDrivers.xml, templates.xml, AnnotationType.java, GrepConsole.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, lightEdit.xml, wsl.distributions.xml, material_theme.xml, vcs.xml, codota.xml, mybatis.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @ClassName: ${NAME}
+ * @Description: TODO
+ * @Date: ${DATE} ${HOUR}:${MINUTE} 
+ * @Author: l_y
+ */
+public @interface ${NAME} {
+}
